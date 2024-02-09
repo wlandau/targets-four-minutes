2b24c4bf37f823e76be10a53fe9621d3ef5bcfd2
wlandau <will.landau.oss@gmail.com>
2024-02-08 20:46:44 -0500

    Update _targets.R

--- _targets.R
+++ _targets.R
@@ -1,28 +1,45 @@
-# Written by use_targets().
+# Created by use_targets().
 # Follow the comments below to fill in this target script.
 # Then follow the manual to check and run the pipeline:
-#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline # nolint
+#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline
 
 # Load packages required to define the pipeline:
 library(targets)
-# library(tarchetypes) # Load other packages as needed. # nolint
+# library(tarchetypes) # Load other packages as needed.
 
 # Set target options:
 tar_option_set(
-  packages = c("dplyr", "ggplot2", "readr"), # packages that your targets need to run
-  format = "rds" # default storage format
-  # Set other options as needed.
+  packages = c("dplyr", "ggplot2", "readr") # packages that your targets use
+  # format = "qs", # Optionally set the default storage format. qs is fast.
+  #
+  # Workflows that take a long time to run may benefit from
+  # optional distributed computing. To use this capability
+  # in tar_make(), supply a {crew} controller
+  # as discussed at https://books.ropensci.org/targets/crew.html.
+  # Choose a controller that suits your needs. For example, the following
+  # sets a controller with 2 workers which will run as local R processes:
+  #
+  #   controller = crew::crew_controller_local(workers = 2)
+  #
+  # Alternatively, if you want workers to run on a high-performance computing
+  # cluster, select a controller from the {crew.cluster} package. The following
+  # example is a controller for Sun Grid Engine (SGE).
+  # 
+  #   controller = crew.cluster::crew_controller_sge(
+  #     workers = 50,
+  #     # Many clusters install R as an environment module, and you can load it
+  #     # with the script_lines argument. To select a specific verison of R,
+  #     # you may need to include a version string, e.g. "module load R/4.3.0".
+  #     # Check with your system administrator if you are unsure.
+  #     script_lines = "module load R"
+  #   )
+  #
+  # Set other tar_option_set() options as needed.
 )
 
-# Configure the backend of tar_make_clustermq() (recommended):
-options(clustermq.scheduler = "multicore")
-
-# Configure the backend of tar_make_future() (optional):
-future::plan(future.callr::callr)
-
-# Load the R scripts with your custom functions:
-lapply(list.files("R", full.names = TRUE), source)
-# source("other_functions.R") # Source other scripts as needed. # nolint
+# Run the R scripts in the R/ folder with your custom functions:
+tar_source()
+# source("other_functions.R") # Source other scripts as needed.
 
 # Replace the target list below with your own:
 list(
